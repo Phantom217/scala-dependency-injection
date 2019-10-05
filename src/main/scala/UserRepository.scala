bdd7c474b7b44a509cc9f45044b93d425c9bbd3f
Tyler Oalman <tyleroalman@gmail.com>
2019-10-04 23:51:40 -0500

    2 - Wrap in an enclosing trait and instantiate here
    First wrap the `UserRepository` in an enclosing trait and instantiate
    the user repository there. This creates a namespace for our repository.
    Next, in order to declare that we would like to have the
    `userRepository` instance injected in the `UserService` we will first do
    what was done to the other repository; wrap it in an enclosing
    (namespace) trait and use a "self-type annotation" to declare our need
    for the `UserRepository` service.

--- src/main/scala/UserRepository.scala
+++ src/main/scala/UserRepository.scala
@@ -1,10 +1,11 @@
-// a dummy service that is not persisting anything
-// solely prints out info
-class UserRepository {
-  def authenticate(user: User): User = {
-    println("authenticating user: " + user)
-    user
-   }
-  def create(user: User) = println("creating user: " + user)
-  def delete(user: User) = println("deleting user: " + user)
+trait UserRepositoryComponent {
+  val userRepository = new UserRepository
+  class UserRepository {
+    def authenticate(user: User): User = {
+      println("authenticating user: " + user)
+      user
+     }
+    def create(user: User) = println("creating user: " + user)
+    def delete(user: User) = println("deleting user: " + user)
+  }
 }
--- src/main/scala/UserService.scala
+++ src/main/scala/UserService.scala
@@ -1,10 +1,14 @@
-class UserService {
-  def authenticate(username: String, password: String): User =
-    userRepository.authenticate(username, password)
+// using self-type annotation declaring the dependencies this
+// component requires, in our case the `UserRepositoryComponent`
+trait UserServiceComponent { this: UserRepositoryComponent =>
+  val userService = new UserService
+  class UserService {
+    def authenticate(username: String, password: String): User =
+      userRepository.authenticate(username, password)
 
-  def create(username: String, password: String) =
-    userRepository.create(new User(username, password))
+    def create(username: String, password: String) =
+      userRepository.create(new User(username, password))
 
-  def delete(user: User) = All is statically typed.
-    userRepository.delete(user)
+    def delete(user: User) = userRepository.delete(user)
+  }
 }
